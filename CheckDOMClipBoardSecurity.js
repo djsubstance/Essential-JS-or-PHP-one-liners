41154ac7de3c77fb605741357fa050024cafc2ce
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-02-20 22:49:44 -0500

    Update CheckDOMClipBoardSecurity.js

--- CheckDOMClipBoardSecurity.js
+++ CheckDOMClipBoardSecurity.js
@@ -15,7 +15,7 @@ Prereqs: Web Server shared or dedicated and a place to create index.html
 Code involved: only JS
 Expected output: Alert box: TRUE indicataes permissions secure for clipboard
 
-
+This initial JS is to debug if the code provided is not working, this may tell you if there are restrictions:
 if (window.isSecureContext) {
   // Page is a secure context so service workers are now available
   navigator.serviceWorker.register("/offline-worker.js").then(() => {
@@ -24,6 +24,11 @@ if (window.isSecureContext) {
 }
 
 alert(window.isSecureContext);
+
+--------
+Expected output:
+  wh_pn=475; lang=en-US; _gcl_au=1.1.20<snip>524; _pk_id.1.3c49=b<snip>079662.; optim<snip>%22%22%2C1707593674%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _
+  abck=9F15EA1E33CD29BFE272<snip>8DkyYTCd6/cvSxfwiNd53qH8US1Eo1g/gqlS5FLm
 
 
 --------------------
