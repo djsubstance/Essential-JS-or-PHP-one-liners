1927dba6f1dc3c53c1cfa3a38552b0ff6232d873
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-04-08 23:24:59 -0400

    Update DumpAllBrowserStoreNfo_upd.js

--- DumpAllBrowserStoreNfo_upd.js
+++ DumpAllBrowserStoreNfo_upd.js
@@ -1,3 +1,54 @@
+Filename: DumpAllBrowserStoreNfo.txt
+Desc: Made for Inspect Console ( to paste in ) but will work as normal JS obviously
+Purpose: To expose in a easy way to hijack and send to other resources (potentially) the session data cookie etc.
+Original Creation Date: 02/04/24
+Last Update: 04/08/24
+By: substance
+Note: Multiple revisions are listed here as i was tweaking it to make it more efficient, the latest version is at the top:
+
+Latest version (v1.2a)
+---------------------------- NOT ONE LINER - EXPANDED FOR CLARITY -----------------------
+console.log(
+  `Browser Local Storage:\n${
+    localStorage.length
+      ? JSON.stringify([...Array(localStorage.length)].map((_, i) => 
+          [localStorage.key(i), localStorage.getItem(localStorage.key(i))])
+          .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}), null, 2)
+      : "None Found"
+  }\n\n[*] Session Storage:\n${
+    sessionStorage.length
+      ? JSON.stringify([...Array(sessionStorage.length)].map((_, i) => 
+          [sessionStorage.key(i), sessionStorage.getItem(sessionStorage.key(i))])
+          .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}), null, 2)
+      : "None Found"
+  }\n\n-------------\n[*] Cookies:\n\t${
+    document.cookie
+      ? `{${document.cookie.split(';').map(cookie => 
+          `\n  "${cookie.split('=')[0].trim()}": "${cookie.split('=')[1]}"`).join(',')}\n\t}`
+      : "None Found"
+  }\n-------------\n`
+);
+
+---------------   ONE LINER :: ONCE YOU UNDERSTAND IT ----------------
+console.log(`Browser Local Storage: ${localStorage.length ? JSON.stringify([...Array(localStorage.length)].map((_, i) => [localStorage.key(i), localStorage.getItem(localStorage.key(i))]).reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}), null, 2) : "None Found"} [*] Session Storage: ${sessionStorage.length ? JSON.stringify([...Array(sessionStorage.length)].map((_, i) => [sessionStorage.key(i), sessionStorage.getItem(sessionStorage.key(i))]).reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {}), null, 2) : "None Found"} ------------- [*] Cookies: ${document.cookie ? `{${document.cookie.split(';').map(cookie => `"${cookie.split('=')[0].trim()}": "${cookie.split('=')[1]}"`).join(', ')}}` : "None Found"} -------------`);
+
+Example output from oneliner (v1.2 updated)
+Note: you should see 3 potentially very large sets of data. Make sure you can determine where one begins and one ends:
+
+The last section is session storage - for this page I see :
+[*] Session Storage: {
+  "soft-nav:latest-mechanism": "react",
+  "soft-nav:react-app-name": "react-code-view",
+  "soft-nav:marker": "0",
+  "soft-nav:referrer": "https://github.com/djsubstance/Essential-JS-or-PHP-one-liners/edit/main/DumpAllBrowserStoreNfo_upd.js"
+}
+.. there are some limitations depending on the page your on in terms of getting this data into an array/variable and "sending" it to
+other resources off the same origin
+
+  
+
+
+
 console.log(
   `%cBrowser Local Storage:\n%c${JSON.stringify([...Array(localStorage.length)].map((_, i) => [localStorage.key(i), localStorage.getItem(localStorage.key(i))]).reduce((acc, [key, value]) => ({...acc, [key]: value}), {}), null, 2)}\n\n%cSession Storage:\n%c${JSON.stringify([...Array(sessionStorage.length)].map((_, i) => [sessionStorage.key(i), sessionStorage.getItem(sessionStorage.key(i))]).reduce((acc, [key, value]) => ({...acc, [key]: value}), {}), null, 2)}\n\n%cCookies:\n%c{${document.cookie.split(';').map(cookie => `\n  "${cookie.split('=')[0].trim()}": "${cookie.split('=')[1]}"`).join(',')}\n}`,
   "color: lightcyan", "", "color: lightcyan", "", "color: lightcyan", ""
