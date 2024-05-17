c71a64c84a3197e57c07300faf8cae7dcaab232b
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-05-17 03:57:44 -0400

    Create DisplayAllElements.js

---
+++ DisplayAllElements.js
@@ -0,0 +1,66 @@
+// change div to html or class .. etc past einto console
+var x = document.querySelectorAll("div");
+var i;
+for (i = 0; i < x.length; i++) {
+    x[i].innerHTML = "";
+} console.log(x);
+
+
+
+output:
+NodeList [html.html__responsive]
+0
+: 
+html.html__responsive
+accessKey
+: 
+""
+ariaAtomic
+: 
+null
+ariaAutoComplete
+: 
+null
+ariaBrailleLabel
+: 
+: 
+StylePropertyMap {size: 0}
+attributes
+: 
+NamedNodeMap {0: itemscope, 1: itemtype, 2: class, 3: lang, itemscope: itemscope, itemtype: itemtype, class: class, lang: lang, length: 4}
+autocapitalize
+: 
+onwebkitanimationend
+: 
+null
+onwebkitanimationiteration
+: 
+null
+onwebkitanimationstart
+: 
+null
+onwebkitfullscreenchange
+: 
+null
+onwebkitfullscreenerror
+: 
+null
+onwebkittransitionend
+: 
+null
+onwheel
+: 
+null
+outerHTML
+: 
+"<html itemscope=\"\" itemtype=\"https://schema.org/QAPage\" class=\"html__responsive \" lang=\"en\"><head></head><body></body></html>"
+outerText
+: 
+""
+ownerDocument
+: 
+: 
+DOMStringList
+assign
+: 
+ƒ assign()
