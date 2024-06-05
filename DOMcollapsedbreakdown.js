f397f24efd0912d16b1e9df52b1d24bded78f39c
9x Datakit Network / Substance <jcat42o@gmail.com>
2024-06-05 04:18:32 -0400

    Create DOMcollapsedbreakdown.js

---
+++ DOMcollapsedbreakdown.js
@@ -0,0 +1,14 @@
+Paste in to console:
+
+  document.querySelectorAll("*").forEach(el => el.innerHTML = "html");console.log(document.querySelectorAll("*"));
+
+
+to console it should display:
+
+NodeList(3) [html, head, body]
+0: html
+1: head
+2: body length: 
+3 [[Prototype]] : NodeList
+
+Each can be expanded 
